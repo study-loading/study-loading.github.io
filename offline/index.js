876ba2db8ebe9985b8d70920b517d324eca89d90
study-loading <wangyong709406@163.com>
2018-07-31 18:14:07 +0800

    init

---
+++ offline/index.js
@@ -0,0 +1,23 @@
+window.addEventListener('load', function () {
+	console.log('serviceWorker registering');
+	navigator.serviceWorker.register(location.pathname + '/sw.js').then(function(registration) {
+		console.log(registration);
+		console.log('ServiceWorker registration successful with scope: ', registration.scope);
+	}).catch(function(err) {
+		console.log('ServiceWorker registration failed: ', err);
+	});
+}, false);
+
+
+
+// 	sendMessage: function (data) {
+// 		console.log(navigator.serviceWorker.controller)
+// 		navigator.serviceWorker.controller && navigator.serviceWorker.controller.postMessage(data);
+// 	}
+// }
+
+// navigator.serviceWorker.addEventListener('message', function (event) {
+//     if (e.data === 'sw.update') {
+//         // 此处可以操作页面的 DOM 元素啦
+//     }
+// });
