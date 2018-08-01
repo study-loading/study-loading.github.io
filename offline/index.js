edd7811acc9d3b641cf0bcf4c604ac378cd4c4de
study-loading <wangyong709406@163.com>
2018-08-01 16:57:08 +0800

    Update index.js

--- offline/index.js
+++ offline/index.js
@@ -30,7 +30,35 @@ function askPermission() {
     });
 }
 
-askP
+askPermission();
+
+var dfdPrompt = null;
+var button = document.getElementById('btn');
+
+window.addEventListener('beforeinstallprompt', function (e) {
+    // 存储事件
+    dfdPrompt = e;
+    // 显示按钮
+    button.style.display = 'block';
+    // 阻止默认事件
+    e.preventDefault();
+    return false;
+});
+
+button.addEventListener('click', function (e) {
+    if (dfdPrompt == null) {
+        return;
+    }
+    // 通过按钮点击事件触发横幅显示
+    dfdPrompt.prompt();
+    // 监控用户的安装行为
+    dfdPrompt.userChoice.then(function (choiceResult) {
+        alert(choiceResult.outcome);
+    });
+    // 隐藏按钮
+    button.style.display = 'none';
+    dfdPrompt = null;
+});
 
 
 
